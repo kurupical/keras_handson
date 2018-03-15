dfafe09e98c8cf2f564e014b37570b7b2e970be1
kurupical <kurupical@gmail.com>
2018-03-15 21:27:11 +0900

    学習済NNを使った推論をできるよう対応

---
+++ handson_keras_predict.py
@@ -0,0 +1,66 @@
+#=========================================
+#
+# handson_kerasで学習したモデルを使って
+# 自分で作成した手書き文字を認識するプログラム
+# (精度は高くない)
+#
+# フォルダ構成
+#
+# keras_predict.py
+# model.json
+# weights.hdf5
+# image
+#  |- *.jpg (ペイントなどで書いた手書き数字をここにいれてね)
+#=========================================
+
+import keras
+from keras.models import model_from_json
+from keras.optimizers import RMSprop
+
+from PIL import Image # pip install Pillowが必要
+import numpy as np
+import glob # pip install globが必要(かも)
+
+model_filename = "model.json"
+weight_filename = "weights.hdf5"
+
+#========================
+# 学習済NNのロード
+#========================
+# モデルのロード
+model = model_from_json(open(model_filename).read())
+
+# 学習済weight,biasのロード
+model.load_weights(weight_filename)
+
+# モデルのコンパイル
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+#========================
+# 画像の取込み
+#========================
+files = glob.glob("image/*.jpg")
+
+for filename in files:
+    # 画像を読み込み
+    img = Image.open(filename)
+
+    # 学習モデルにあわせて、画像を28*28に圧縮
+    img_resize = img.resize((28, 28))
+
+    # 学習モデルにあわせて、画像をグレースケールに
+    img_gray = img_resize.convert('L')
+
+    # 数値に変換
+    data = np.array(img_gray).reshape(1, 784)
+
+    # 黒ぬき画像→白ぬき画像に変換
+    data = 255 - data
+
+    #========================
+    # 画像の推論
+    #========================
+    result = model.predict_proba(data)
+    print("filename={}, result={}".format(filename,result))
